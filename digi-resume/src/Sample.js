982f7a829218e124881fb26e1968c0cdd7ee43f6
manosundarmanivel <manosundarmanivel@gmail.com>
2023-08-30 22:11:11 +0530

    Building UI

--- digi-resume/src/Sample.js
+++ digi-resume/src/Sample.js
@@ -2,61 +2,61 @@ import React from "react";
 
 const Sample = () => {
   return (
-    <div>
+    <div className="font-poppins">
       <div>
         <div className="flex">
-          <img />
+          <img src="https://wallpapers.com/images/hd/cool-profile-picture-87h46gcobjl5e4xu.jpg" className="h-20 rounded-full" />
           <div>
-            <h1></h1>
-            <h1></h1>
+            <h1>Sally Robins</h1>
+            <h1>NEW YORK</h1>
             <div className="flex">
               <div>
-                <h1></h1>
-                <h1></h1>
+                <h1>0</h1>
+                <h1>MY COURSES</h1>
               </div>
                
               <div>
-                <h1></h1>
-                <h1></h1>
+                <h1>2</h1>
+                <h1>FOLLOWERS</h1>
               </div>
               <div>
-                <h1></h1>
-                <h1></h1>
+                <h1>10</h1>
+                <h1>FOLLOWING</h1>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <h1>TS</h1>
+          <h1>Total Statistics</h1>
           <div className="flex">
             <div>
               <div></div>
-              <h1></h1>
-              <h1></h1>
+              <h1>FINISHED COURSES</h1>
+              <h1>3</h1>
             </div>
             <div>
               <div></div>
-              <h1></h1>
-              <h1></h1>
+              <h1>FINISHED COURSES</h1>
+              <h1>4</h1>
             </div>
             <div>
               <div></div>
-              <h1></h1>
-              <h1></h1>
+              <h1>FINISHED COURSES</h1>
+              <h1>6</h1>
             </div>
           </div>
         </div>
         <div>
-          <h1>A</h1>
+          <h1>Achievements</h1>
           <div>
             <div className="flex">
               <div>
                 <h1>icon</h1>
               </div>
               <div>
-                <h1></h1>
-                <h1></h1>
-                <h1></h1>
+                <h1>Committed Learner</h1>
+                <h1>Progress</h1>
+                <h1>Reach a 3 day streak</h1>
               </div>
             </div>
             <div className="flex">
@@ -64,9 +64,9 @@ const Sample = () => {
                 <h1>icon</h1>
               </div>
               <div>
-                <h1></h1>
-                <h1></h1>
-                <h1></h1>
+                <h1>Committed Learner</h1>
+                <h1>Progress</h1>
+                <h1>Reach a 3 day streak</h1>
               </div>
             </div>
             <div className="flex">
@@ -74,11 +74,13 @@ const Sample = () => {
                 <h1>icon</h1>
               </div>
               <div>
-                <h1></h1>
-                <h1></h1>
-                <h1></h1>
+                <h1>Committed Learner</h1>
+                <h1>Progress</h1>
+                <h1>Reach a 3 day streak</h1>
               </div>
             </div>
+            
+            
           </div>
         </div>
       </div>
