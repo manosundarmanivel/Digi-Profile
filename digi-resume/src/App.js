774904e8f30fbff46d2327963c62fa6a832995b4
manosundarmanivel <manosundarmanivel@gmail.com>
2023-09-04 23:09:47 +0530

    building base

--- digi-resume/src/App.js
+++ digi-resume/src/App.js
@@ -6,11 +6,12 @@ import LeetCode from './LeetCode';
 import Dashboard from './Dashboard';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+import Home from './Home';
 
 function App() {
   return (
     <div className='font-poppins'>
-     <SignIn/>
+    <Home/>
       {/* <LeetCode/>
       <Dashboard/> */}
     </div>
--- digi-resume/src/Home.js
+++ digi-resume/src/Home.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+     <div className='flex'>
+      <h1>Logo</h1>
+      <div className='border border-black p-3'>
+        <h1>Try for free</h1>
+      </div>
+     </div>
+     <div className='flex'>
+      <div>
+        <h1>Bring your work & team together</h1>
+        <h1>Work fast and flexible by communicating effectively.</h1>
+        <button>Sign up now</button>
+      </div>
+      <img/>
+
+     </div>
+     <div>
+      <h1>"We love using Teamio. Our efficiency grew by 45% and that's amazing!"</h1>
+      <div className='flex'>
+        <img/>
+        <h1>Mano Sundar , Company CEO</h1>
+      </div>
+     </div>
+     <div className='flex justify-around'>
+      <div></div>
+      <div className='flex'>
+        <div>
+          <h1>Product</h1>
+          <h1>Product</h1>
+          <h1>Product</h1>
+          <h1>Product</h1>
+        </div>
+        <div>
+          <h1>Product</h1>
+          <h1>Product</h1>
+          <h1>Product</h1>
+          <h1>Product</h1>
+        </div>
+      </div>
+     </div>
+      </div>
   )
 }
 
