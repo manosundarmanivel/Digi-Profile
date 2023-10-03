edeff7bad64aebae8f4d05923c249a5eced33290
manosundarmanivel <manosundarmanivel@gmail.com>
2023-10-03 17:53:29 +0530

    small update

--- digi-resume/src/Pages/Home.js
+++ digi-resume/src/Pages/Home.js
@@ -21,13 +21,13 @@ const Home = () => {
   }
   return (
     <div className="">
-      <div className="flex justify-between items-center px-20 py-3 bg-slate-400 ">
+      <div className="flex justify-between items-center px-20 py-3 ">
         <h1 className="text-[30px] font-extrabold">Profilio<span className="text-[#f6a130]">.in</span></h1>
         <div className="border  border-[#323234] p-3">
           <h1>Try for free</h1>
         </div>
       </div>
-      <div className=" justify-between items-center text-center  bg-[#fdebcf] px-20">
+      <div className=" justify-between items-center   flex bg-[#fdebcf] px-20 py-20">
         <div className="">
           <h1 className="font-bold text-centre text-[60px] text-[#323234] ">
             Showcase Your Code <br />
@@ -39,14 +39,14 @@ const Home = () => {
           <div className="m-5">
           <button onClick={()=>{
             handleSignInWithGoogle();
-          }} className="p-3 mt-12 px-12 mt-3 mx-2 rounded-lg bg-[#323234]  text-white text-[20px]">
-            Get Started its free
+          }} className="p-3 mt-12 px-12 mt-3 mx-2  border border-black   text-[20px]">
+            SignUp
           </button>
-          <button onClick={()=>{
+          {/* <button onClick={()=>{
             handleSignInWithGoogle();
           }} className="p-3 mt-12 px-12 mt-3  mx-2 rounded-lg bg-[#323234]  text-white text-[20px]">
             Get in touch
-          </button>
+          </button> */}
           </div>
           
         </div>
@@ -54,7 +54,7 @@ const Home = () => {
         <img src={HomePic} className="w-[400px] " />
         </div>
       </div>
-      <div className="p-5 justify-center text-center ">
+      {/* <div className="p-5 justify-center text-center ">
         <h1 className="text-[20px]">
           "We love using Profilio.in . Our efficiency grew <br/>by 45% and that's amazing!"
         </h1>
@@ -62,7 +62,7 @@ const Home = () => {
           <img src="" />
           <h1>Mano Sundar , Company CEO</h1>
         </div>
-      </div>
+      </div> */}
       <Footer/>
     </div>
   );
