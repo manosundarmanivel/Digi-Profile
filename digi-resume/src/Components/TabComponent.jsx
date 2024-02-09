5adf4ffe9c2174e03ce0f9944ac318d2e5baabd5
manosundar <manosundarmanivel@gmail.com>
2024-02-09 08:34:24 +0530

    updating profile page

--- digi-resume/src/Components/TabComponent.jsx
+++ digi-resume/src/Components/TabComponent.jsx
@@ -6,10 +6,11 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Badge from "../../node_modules/@mui/material/Badge/Badge";
 import leet from "../Assets/img/leetcode.png";
-import hack from "../Assets/img/hackerrank_logo.png"
-import hackearth from "../Assets/img/HackerEarth.png"
-import topcoder from "../Assets/img/topcoder.png"
+import hack from "../Assets/img/hackerrank_logo.png";
+import hackearth from "../Assets/img/HackerEarth.png";
+import topcoder from "../Assets/img/topcoder.png";
 import LeetCode from "../Pages/LeetCode";
+import github from "../Assets/img/git.png"
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -66,168 +67,205 @@ export default function BasicTabs() {
           <Tab label="Achievements" {...a11yProps(4)} />
         </Tabs>
       </Box>
-      <CustomTabPanel value={value} index={0}>
-        
-       
-      </CustomTabPanel>
+      <CustomTabPanel value={value} index={0}></CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-      <div className="flex flex-wrap  justify-around">
-        <div className="border p-3 m-3">
-          <div className="flex items-center">
-            <img src={leet} width={35} />
-            <h1 className="font-semibold p-1">LeetCode</h1>
-          </div>
-          <div >
-            <div className="flex justify-between">
-              <div className="border p-2 m-3 w-[200px]">
-                <h1>Total Solved</h1>
-                <h1>NA</h1>
-              </div>
-              <div >
-                <div className="border p-2 m-3">
-                  <h1>Active </h1>
-                  <h1>NA</h1>
-                </div>
-                <div className="border p-2 m-3">
-                  <h1>Streak </h1>
-                  <h1>NA</h1>
-                </div>
-              </div>
+        <div className="flex flex-wrap  justify-around">
+          <div className="border p-3 m-3">
+            <div className="flex items-center">
+              <img src={leet} width={35} />
+              <h1 className="font-semibold p-1">LeetCode</h1>
             </div>
-            <div className="flex">
-              <div className="border p-2 m-3">
-                <h1>Rank </h1>
-                <h1>NA</h1>
+            <div>
+              <div className="flex justify-between">
+                <div className="border p-2 m-3 w-[200px]">
+                  <h1>Total Solved</h1>
+                  <h1>NA</h1>
+                </div>
+                <div>
+                  <div className="border p-2 m-3">
+                    <h1>Active </h1>
+                    <h1>NA</h1>
+                  </div>
+                  <div className="border p-2 m-3">
+                    <h1>Streak </h1>
+                    <h1>NA</h1>
+                  </div>
+                </div>
               </div>
-              <div className="border p-2 m-3">
-                <h1>Badge </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Submissions </h1>
-                <h1>NA</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border p-3 m-3">
-          <div className="flex items-center">
-            <img src={hack} width={140} className="m-3" />
-            
-          </div>
-          <div >
-            <div className="flex justify-between">
-              <div className="border p-2 m-3 w-[200px]">
-                <h1>Total Solved</h1>
-                <h1>NA</h1>
-              </div>
-              <div >
+              <div className="flex">
                 <div className="border p-2 m-3">
-                  <h1>Active </h1>
+                  <h1>Rank </h1>
                   <h1>NA</h1>
                 </div>
                 <div className="border p-2 m-3">
-                  <h1>Streak </h1>
+                  <h1>Badge </h1>
+                  <h1>NA</h1>
+                </div>
+                <div className="border p-2 m-3">
+                  <h1>Submissions </h1>
                   <h1>NA</h1>
                 </div>
               </div>
             </div>
-            <div className="flex">
-              <div className="border p-2 m-3">
-                <h1>Rank </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Badge </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Submissions </h1>
-                <h1>NA</h1>
-              </div>
+          </div>
+          <div className="border p-3 m-3">
+            <div className="flex items-center">
+              <img src={hack} width={140} className="m-3" />
             </div>
-          </div>
-        </div>
-        <div className="border p-3 m-3">
-          <div className="flex items-center">
-            <img src={hackearth} width={120} className="m-2" />
-            
-          </div>
-          <div >
-            <div className="flex justify-between">
-              <div className="border p-2 m-3 w-[200px]">
-                <h1>Total Solved</h1>
-                <h1>NA</h1>
+            <div>
+              <div className="flex justify-between">
+                <div className="border p-2 m-3 w-[200px]">
+                  <h1>Total Solved</h1>
+                  <h1>NA</h1>
+                </div>
+                <div>
+                  <div className="border p-2 m-3">
+                    <h1>Active </h1>
+                    <h1>NA</h1>
+                  </div>
+                  <div className="border p-2 m-3">
+                    <h1>Streak </h1>
+                    <h1>NA</h1>
+                  </div>
+                </div>
               </div>
-              <div >
+              <div className="flex">
                 <div className="border p-2 m-3">
-                  <h1>Active </h1>
+                  <h1>Rank </h1>
                   <h1>NA</h1>
                 </div>
                 <div className="border p-2 m-3">
-                  <h1>Streak </h1>
-                  <h1>NA</h1>
-                </div>
-              </div>
-            </div>
-            <div className="flex">
-              <div className="border p-2 m-3">
-                <h1>Rank </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Badge </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Submissions </h1>
-                <h1>NA</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border p-3 m-3">
-          <div className="flex items-center">
-            <img src={topcoder} width={120} className="m-2" />
-            
-          </div>
-          <div >
-            <div className="flex justify-between">
-              <div className="border p-2 m-3 w-[200px]">
-                <h1>Total Solved</h1>
-                <h1>NA</h1>
-              </div>
-              <div >
-                <div className="border p-2 m-3">
-                  <h1>Active </h1>
+                  <h1>Badge </h1>
                   <h1>NA</h1>
                 </div>
                 <div className="border p-2 m-3">
-                  <h1>Streak </h1>
+                  <h1>Submissions </h1>
                   <h1>NA</h1>
                 </div>
               </div>
             </div>
-            <div className="flex">
-              <div className="border p-2 m-3">
-                <h1>Rank </h1>
-                <h1>NA</h1>
+          </div>
+          <div className="border p-3 m-3">
+            <div className="flex items-center">
+              <img src={hackearth} width={120} className="m-2" />
+            </div>
+            <div>
+              <div className="flex justify-between">
+                <div className="border p-2 m-3 w-[200px]">
+                  <h1>Total Solved</h1>
+                  <h1>NA</h1>
+                </div>
+                <div>
+                  <div className="border p-2 m-3">
+                    <h1>Active </h1>
+                    <h1>NA</h1>
+                  </div>
+                  <div className="border p-2 m-3">
+                    <h1>Streak </h1>
+                    <h1>NA</h1>
+                  </div>
+                </div>
               </div>
-              <div className="border p-2 m-3">
-                <h1>Badge </h1>
-                <h1>NA</h1>
-              </div>
-              <div className="border p-2 m-3">
-                <h1>Submissions </h1>
-                <h1>NA</h1>
+              <div className="flex">
+                <div className="border p-2 m-3">
+                  <h1>Rank </h1>
+                  <h1>NA</h1>
+                </div>
+                <div className="border p-2 m-3">
+                  <h1>Badge </h1>
+                  <h1>NA</h1>
+                </div>
+                <div className="border p-2 m-3">
+                  <h1>Submissions </h1>
+                  <h1>NA</h1>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          <div className="border p-3 m-3">
+            <div className="flex items-center">
+              <img src={topcoder} width={120} className="m-2" />
+            </div>
+            <div>
+              <div className="flex justify-between">
+                <div className="border p-2 m-3 w-[200px]">
+                  <h1>Total Solved</h1>
+                  <h1>NA</h1>
+                </div>
+                <div>
+                  <div className="border p-2 m-3">
+                    <h1>Active </h1>
+                    <h1>NA</h1>
+                  </div>
+                  <div className="border p-2 m-3">
+                    <h1>Streak </h1>
+                    <h1>NA</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="border p-2 m-3">
+                  <h1>Rank </h1>
+                  <h1>NA</h1>
+                </div>
+                <div className="border p-2 m-3">
+                  <h1>Badge </h1>
+                  <h1>NA</h1>
+                </div>
+                <div className="border p-2 m-3">
+                  <h1>Submissions </h1>
+                  <h1>NA</h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
-        Item Three
+        <div className="border">
+        <img src={github} width={120} className="m-2 " />
+        <div className="flex  m-2 p-2 justify-around">
+        
+        <div>
+          <div className="border p-2 m-3">
+            <h1>Total Repositories</h1>
+            <h1>NA</h1>
+          </div>
+          <div className="border p-2 m-3">
+            <h1>Recent Commits</h1>
+            <h1>NA</h1>
+          </div>
+        </div>
+        <div>
+          <div className="border p-2 m-3">
+            <h1>Pull Request</h1>
+            <h1>NA</h1>
+          </div>
+          <div className="border p-5 m-3">
+            <h1>Issues</h1>
+            <h1>NA</h1>
+          </div>
+        </div>
+        <div className="border p-2 m-3">
+          <h1>Pinned Repository</h1>
+        </div>
+        <div>
+          <div className="border p-2 m-3">
+            <h1>Followers</h1>
+            <h1>NA</h1>
+          </div>
+          <div className="border p-2 m-3">
+            <h1>Following</h1>
+            <h1>NA</h1>
+          </div>
+          <div className="border p-2 m-3">
+            <h1>Contributions</h1>
+            <h1>NA</h1>
+          </div>
+        </div>
+      </div>
+        </div>
+       
       </CustomTabPanel>
       <CustomTabPanel value={value} index={3}>
         Item Three
